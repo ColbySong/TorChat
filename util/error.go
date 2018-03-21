49d70993690d4b1a7eb4207cb4be44d49b9f0fc0
dchen121 <dchen121@gmail.com>
2018-03-21 10:59:04 -0700

    Add error handling

---
+++ util/error.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	ErrLog *log.Logger = log.New(os.Stderr, "[miner] ", log.Lshortfile|log.LUTC|log.Lmicroseconds)
+	OutLog *log.Logger = log.New(os.Stderr, "[miner] ", log.Lshortfile|log.LUTC|log.Lmicroseconds)
+)
+
+func HandleNonFatalError(msg string, e error) {
+	if e != nil {
+		ErrLog.Printf("[ERROR] %s, err = %s\n", msg, e.Error())
+	}
+}
+
+func HandleFatalError(msg string, e error) {
+	if e != nil {
+		ErrLog.Fatalf("[FATAL ERROR] %s, err = %s\n", msg, e.Error())
+	}
+}
